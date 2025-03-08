149d2f124d2482ab406c329909345d11b5c5847f
MayanPipaliya <pipaliyamayan996@gmail.com>
2025-03-08 22:56:22 +0530

    add

---
+++ event handler/event/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Form from './components/Form'
+import Timer from './components/Timer'
+import Fakeapi from './components/Fakeapi'
+
+const App = () => {
+  return (
+    <div>
+
+      <Form/>
+     <Timer duration={60} />
+     <Fakeapi/>
+    
+    </div>
+  )
+}
+
+export default App
